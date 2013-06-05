875d364b70ce7d38ac80230ca46dfffc718b232e
Jason Knebel <jasonkneb@gmail.com>
2013-06-05 15:59:14 -0400

    breaking out import_members helper

--- app.rb
+++ app.rb
@@ -5,24 +5,24 @@ require 'CSV'
 
 @members = []
 
-import_members('pipe.txt', ' | ', { 
+parse_members('pipe.txt', ' | ', { 
   first_name: 1, last_name: 0, gender: 3, birthday: 5, favorite_color: 4})
-import_members('comma.txt', ', ', { 
+parse_members('comma.txt', ', ', { 
   first_name: 1, last_name: 0, gender: 2, birthday: 4, favorite_color: 3})
-import_members('space.txt', ' ', { 
+parse_members('space.txt', ' ', { 
   first_name: 1, last_name: 0, gender: 3, birthday: 4, favorite_color: 5})
 
 
 outputs=[]
 
 # Output 1 – sorted by gender (females before males) 
-#   then by last name ascending
+# then by last name ascending
 outputs << @members.sort_by{|p| [p.gender, p.last_name] }
 
-# # Output 2 – sorted by birth date, ascending.
+# Output 2 – sorted by birth date, ascending.
 outputs << @members.sort_by{|p| [p.birthday, p.last_name] }
 
-# # Output 3 – sorted by last name, descending.
+# Output 3 – sorted by last name, descending.
 outputs << @members.sort_by(&:last_name).reverse
 
 
--- helpers/import.rb
+++ helpers/import.rb
@@ -1,18 +1,21 @@
-def import_members(file_name, col_sep, order)
+def parse_members(file_name, col_sep, order)
 
   path = 'input_data/' + file_name
 
   CSV.foreach(path, col_sep: col_sep) do |line|
-
-    @members << Person.new(
-      first_name: line[order[:first_name]],
-      last_name: line[order[:last_name]],
-      gender: line[order[:gender]], 
-      birthday: line[order[:birthday]],
-      favorite_color: line[order[:favorite_color]]
-    )
-
+    add_member(line, order)
   end
-
+  
 end
 
+def add_member(line, order)
+
+  @members << Person.new(
+    first_name: line[order[:first_name]],
+    last_name: line[order[:last_name]],
+    gender: line[order[:gender]], 
+    birthday: line[order[:birthday]],
+    favorite_color: line[order[:favorite_color]]
+  )
+
+end
